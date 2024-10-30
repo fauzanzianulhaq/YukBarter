ad8bcff0ed10016cbb90b251f3ea8ef6bee2f04b
fauzanzianulhaq <115859323+fauzanzianulhaq@users.noreply.github.com>
2024-10-30 15:18:34 +0700

    update login

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    function index() {
+
+    }
+    function login(){
+        
+    }
+}
